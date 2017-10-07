9c1ab853210efc21f83f08f3fa96cdf8f0ff36c1
Alex Kopytov <alex.kopytov@gmail.com>
2017-10-07 03:48:40 +0300

    Updated run_jupyter script.

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -3,10 +3,10 @@
 JUPYTER2=/cygdrive/c/Anaconda2/Scripts/jupyter.exe
 JUPYTER3=/cygdrive/c/Anaconda3/Scripts/jupyter.exe
 
-if [ -f "$JUPYTER2" ]; then
-  $JUPYTER2 notebook
-elif [ -f "$JUPYTER3" ]; then
+if [ -f "$JUPYTER3" ]; then
   $JUPYTER3 notebook
+elif [ -f "$JUPYTER2" ]; then
+  $JUPYTER2 notebook
 else
   echo "Jupyter not found."
 fi
