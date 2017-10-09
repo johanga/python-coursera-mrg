31c358cc47a1fd96a94a456a000809b0aa90fc60
Alex Kopytov <alex.kopytov@gmail.com>
2017-10-10 01:59:45 +0300

    Week 1. Programming Assignment: Корни квадратного уравнения.

---
+++ week1/solution3.py
@@ -0,0 +1,12 @@
+import math
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+
+d = b**2 - 4 * a * c
+x1 = (-b - math.sqrt(d))/2*a
+x2 = (-b + math.sqrt(d))/2*a
+
+print(int(x1))
+print(int(x2))
